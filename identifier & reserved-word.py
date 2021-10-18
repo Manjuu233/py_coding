0e319a406b68eb1f14e86ce338d68e0f22fa9c6b
Nagiasu <63838819+Manjuu233@users.noreply.github.com>
2021-10-18 19:52:19 +0800

    identifier & reserved word
    标识符和保留字

---
+++ identifier & reserved-word.py
@@ -0,0 +1,12 @@
+import keyword
+print(keyword.kwlist) #自己跑一下试试
+
+name='Nagisau'
+# name-变量名
+# = -赋值运算符
+# 'Nagiasu'-值
+
+print(name)
+print('标识',id(name)) #内存地址
+print('类型',type(name))
+print('值',name)
