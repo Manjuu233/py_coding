ccf776ce922006eb2114af60f815e79507af0556
Nagiasu <63838819+Manjuu233@users.noreply.github.com>
2021-10-18 11:23:04 +0800

    PTA 2-1
    instance of PTA 2-1
    the second uses a different approach

---
+++ 2-1.py
@@ -0,0 +1,3 @@
+str0 = input()
+str1 = input()
+print(str0,str1,sep="-",end="!")
---
+++ 2-1_2.py
@@ -0,0 +1,5 @@
+str0 = input()
+str1 = input()
+str_out = str0+"-"+str1+"!"
+print(str_out)
+#这个做法没有用到print函数中的sep和end参数
