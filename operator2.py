41d5d4effac0f7bd675ba4c2d0c5aa179e5ce8cd
Nagiasu <63838819+Manjuu233@users.noreply.github.com>
2021-10-27 23:34:31 +0800

    comparison operator
    比较运算符

---
+++ operator2.py
@@ -0,0 +1,20 @@
+a,b=233,666
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+c,d=233,233
+print(a==b) #比value
+print(a is b) #比ID
+print(a is not b) #a的id和b的id是不相等的吗？
+
+lst1=[1,2,3,4,5]
+lst2=[1,2,3,4,5]
+print(lst1==lst2) #比value
+print(lst1 is lst2) #比ID
+print(lst1 is not lst2) #lst1的id和lst2的id是不相等的吗？
+print(id(lst1))
+print(id(lst2))
