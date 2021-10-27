41875009aca7987aace26c84c1dcb99a67ab8d7b
Nagiasu <63838819+Manjuu233@users.noreply.github.com>
2021-10-27 23:19:36 +0800

    assignment operators
    赋值运算符

---
+++ operator.py
@@ -0,0 +1,23 @@
+print(1+1) #加法
+print(3-1) #减法
+print(2*4) #乘法
+print(11/2) #除法
+print(11//2) #整除
+print(11%2) #取余
+print(2**3) #乘方
+
+i=233+666
+print(1)
+
+a=b=c=114514
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+a+=1 #参数赋值
+print(a)
+
+m,n,p=1,2,3 #解包赋值
+print(m,n,p)
+m,n=n,m #用解包赋值交换变量
+print(m,n)
