0226e7c96e4b6658e5c4c5a0aba441b62b65c949
Nagiasu <63838819+Manjuu233@users.noreply.github.com>
2021-10-27 23:58:35 +0800

    Boolean operators
    布尔运算符

---
+++ operator3.py
@@ -0,0 +1,22 @@
+a,b=233,666
+print(a==233 and b==666)
+print(a==233 and b<666)
+print(a!=233 and b==666)
+print(a!=233 and b!=666)
+
+print(a==233 or b==666)
+print(a==233 or b<666)
+print(a!=233 or b==666)
+print(a!=233 or b!=666)
+
+m=True
+n=False
+print(not(m))
+print(not(n))
+
+
+str1='kano_dayo'
+print('kano'in str1)
+print('test'in str1)
+print('kano'not in str1)
+print('test'not in str1)
