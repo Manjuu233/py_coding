a832cce8c9a5e1d742b29592d4f6f0bcbfcddbcc
Nanoda! <manjuu233@outlook.com>
2021-11-03 21:41:24 +0800

    PTA 4-11
    输入一组数据，求平均值。要求：
    数据正确，正确计算。
    数据有错误，能异常处理，输出错误信息，不进行计算。
    
    行1：n代表输入的数据个数
    接下来n行表示输入的数据
    输入数据合法正确，则输出正确，并输出平均值。
    输入数据不正确，如果数值错误，输出数值错误
    除0错误，输出除0错误，n不能等0
    不管有没有异常，都输出程序结束

--- 4-11.py
+++ 4-11.py
@@ -1,7 +1,17 @@
-import random as r
-a, n = map(int, input().split())
-r.seed(a)
-for i in range(n):
-    n1 = r.randint(1, 9)
-    n2 = r.randint(1, 9)
-    print("{0}+{1}=".format(n1, n2))
+try:
+    num=int(input())
+    su = 0
+    for i in range(num):
+        grade = eval(input())
+        su += grade
+    avg  = su / num
+
+except ZeroDivisionError:
+    print("除0错误，n不能等0")
+except:
+    print("数值错误")
+else:
+    print("正确")
+    print("avg={:.2f}".format(su / num))
+finally:
+    print("程序结束")
