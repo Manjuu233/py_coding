60e8ee24d8c584e49411d31c68d41a8cf4d997a8
Nanoda! <manjuu233@outlook.com>
2021-11-03 20:58:14 +0800

    Add files via upload
    为了提倡居民节约用电，某省电力公司执行“阶梯电价”，安装一户一表的居民用户电价分为两个“阶梯”：月用电量50千瓦时（含50千瓦时）以内的，电价为0.53元/千瓦时；超过50千瓦时的，超出部分的用电量，电价上调X元/千瓦时。请编写程序计算电费。
    一行中输入某用户的月用电量（单位：千瓦时）和电价上调的X值（单位：元），中间用空格分开。
    在一行中输出该用户应支付的电费（元），结果保留两位小数，格式如：“cost = 应付电费值”。

---
+++ 4-2.py
@@ -0,0 +1,11 @@
+input=input()
+lst=input.split(" ",1)
+amount=float(lst[0])
+extra=float(lst[1])
+
+if (amount<=50):
+    total=0.53*amount
+    print("cost =","{:.2f}".format(total))
+else:
+    total=(50*0.53)+((amount-50)*(0.53+extra))
+    print("cost =","{:.2f}".format(total))
